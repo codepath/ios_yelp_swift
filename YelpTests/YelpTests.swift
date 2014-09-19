b64efc1caac9ef558c723c2e099f281bf8b7f8e3
Timothy Lee <tim@codepath.com>
2014-09-19 16:55:38 -0700

    Initial Commit

---
+++ YelpTests/YelpTests.swift
@@ -0,0 +1,36 @@
+//
+//  YelpTests.swift
+//  YelpTests
+//
+//  Created by Timothy Lee on 9/19/14.
+//  Copyright (c) 2014 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class YelpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
