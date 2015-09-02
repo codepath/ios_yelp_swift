61d415b53805d76573553d4879015c50372d872a
David Sklar <sklard@tumblr-MacBookPro-f02af3.local>
2015-09-02 15:58:57 -0700

    start of the search filter

---
+++ Yelp/SearchFilterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchFilterViewController.swift
+//  Yelp
+//
+//  Created by David Sklar on 9/2/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+// import Cocoa
+
+class SearchFilterViewController: UIViewController {
+
+}
