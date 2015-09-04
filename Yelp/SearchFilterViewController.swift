7063a5a4aa98d28c1c0fb02e9a8b462e694d9eee
David Sklar <sklard@UNKNOWN-10-75-141-X.yahoo.com>
2015-09-04 14:29:52 -0700

    Finally: categories are working

--- Yelp/SearchFilterViewController.swift
+++ Yelp/SearchFilterViewController.swift
@@ -17,23 +17,45 @@ class SearchFilterViewController: UIViewController, UITableViewDelegate, UITable
 
     
  
-    var categories = [
+    var categories : Dictionary<String,[(String)]> = [
         "Afghan": ["afghani"],
         "African": ["african"],
         "American, new": ["newamerican"],
         "American, traditional": ["tradamerican"],
         "Arabian": ["arabian"],
-        "Argentine": ["argentine"]
+        "Argentine": ["argentine"],
+        "Armenian": ["armenian"],
+        "Asian Fusion": ["asianfusion"],
+        "Asturian": ["asturian"],
+        "Australian": ["australian"],
+        "Austrian": ["austrian"],
+        "Baguettes": ["baguettes"],
+        "Bangladeshi": ["bangladeshi"],
+        "Barbeque": ["bbq"],
+        "Basque": ["basque"],
+        "Bavarian": ["bavarian"],
+        "Beer Garden": ["beergarden"],
+        "Beer Hall": ["beerhall"]
     ]
-
+    
+    var sortedCatNames : [String]?
     
 
+    // I'm trying to use native multiple-selection support:
+    // http://stackoverflow.com/questions/3040894/uitableview-multiple-selection
+    // but perhaps allowsMultipleSelection is deprecated?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sortedCatNames = (Array(self.categories.keys)).sorted { $0.localizedCaseInsensitiveCompare($1) == NSComparisonResult.OrderedAscending }
+
+        
         self.tableFoodCategory.dataSource = self
         self.tableFoodCategory.delegate = self
+        
+        self.tableFoodCategory.allowsMultipleSelection = true
         
         self.tableFoodCategory.rowHeight = UITableViewAutomaticDimension
         self.tableFoodCategory.estimatedRowHeight = 120
@@ -55,7 +77,7 @@ class SearchFilterViewController: UIViewController, UITableViewDelegate, UITable
         println("CELLFORROW")
         println(indexPath.row)
         println(Array(categories.keys)[indexPath.row])
-        cell.title.text = Array(categories.keys)[indexPath.row] as String
+        cell.title.text = self.sortedCatNames![indexPath.row]
         
         return cell
 
