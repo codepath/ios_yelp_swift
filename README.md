6d4a7a5fc7061a9e6fe287d01ac7f3e1b0b06172
Marcel Weekes <weekes@gmail.com>
2016-02-12 17:53:03 -0800

    Add Search Bar to the navigation bar

--- README.md
+++ README.md
@@ -38,7 +38,8 @@ This is a personal todo list as I step through the assignment.
 - [x] Project setup: fork repo and build headless client
 - [x] Add tableView with basic prototype cells
 - [x] Use AutoLayout to configure custom cells
-- [ ] Add Search Bar in the navigation bar
+- [x] Add Search Bar in the navigation bar
+- [ ] Add Settings ViewController
 
 <!---
 ***
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -12,26 +12,29 @@ class BusinessesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    // TODO: replace with BusinessSearchSettings
+    var searchString: String?
+    var searchBar: UISearchBar!
     var businesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // UITableView
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
-
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
-
-            self.tableView.reloadData()
         
-            for business in businesses {
-                print(business.name!)
-                print(business.address!)
-            }
-        })
+        // BusinessSearchSettings
+        searchString = "Thai"
+        searchBusinesses()
 
 /* Example of Yelp search with more search options specified
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
@@ -59,6 +62,19 @@ class BusinessesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private func searchBusinesses() {
+        Business.searchWithTerm(searchString!, completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            
+            self.tableView.reloadData()
+            
+            for business in businesses {
+                print(business.name!)
+                print(business.address!)
+            }
+        })
+    }
 
 }
 
@@ -85,3 +101,31 @@ extension BusinessesViewController: UITableViewDataSource {
 extension BusinessesViewController: UITableViewDelegate {
     
 }
+
+extension BusinessesViewController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchString = searchBar.text
+        searchBusinesses()
+        searchBar.resignFirstResponder()
+    }
+}
+
+
+
+
+
