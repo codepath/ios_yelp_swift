c3d91fb3a5337cef1e8e7375fe6fb664933de2b9
Michael <realq86@gmail.com>
2016-10-18 18:24:53 -0700

    Resolve warning of unused return by searchWithTerm()

--- Yelp/Business.swift
+++ Yelp/Business.swift
@@ -85,10 +85,10 @@ class Business: NSObject {
     }
     
     class func searchWithTerm(term: String, completion: @escaping ([Business]?, Error?) -> Void) {
-        YelpClient.sharedInstance.searchWithTerm(term, completion: completion)
+        _ = YelpClient.sharedInstance.searchWithTerm(term, completion: completion)
     }
     
     class func searchWithTerm(term: String, sort: YelpSortMode?, categories: [String]?, deals: Bool?, completion: @escaping ([Business]?, Error?) -> Void) -> Void {
-        YelpClient.sharedInstance.searchWithTerm(term, sort: sort, categories: categories, deals: deals, completion: completion)
+        _ = YelpClient.sharedInstance.searchWithTerm(term, sort: sort, categories: categories, deals: deals, completion: completion)
     }
 }
